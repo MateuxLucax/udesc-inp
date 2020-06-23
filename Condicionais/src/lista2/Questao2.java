a7a3c523bcc517644be9b0169ad4cd170dc69615
Mateux <mateusslucass840@gmail.com>
2020-06-22 21:16:48 -0300

    Condicionais

---
+++ Condicionais/src/lista2/Questao2.java
@@ -0,0 +1,18 @@
+package lista2;
+
+import java.util.Scanner;
+
+public class Questao2 {
+	
+	public static void main(String []args){
+
+        int opcao, valor1, valor2, valor3;
+
+        Scanner scan = new Scanner(System.in);		
+        
+        System.out.println("Insira um número inteiro: ");
+        valor1 = scan.nextInt();
+        
+        scan.close();
+	}
+}
