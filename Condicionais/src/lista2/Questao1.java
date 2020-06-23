978590737877ba7e430db37eeb10ba63935b12c5
Mateux <mateusslucass840@gmail.com>
2020-06-23 20:45:01 -0300

    2° List finished

--- Condicionais/src/lista2/Questao1.java
+++ Condicionais/src/lista2/Questao1.java
@@ -3,34 +3,54 @@ package lista2;
 import java.util.Scanner;
 
 public class Questao1 {
-	
-	public static void main(String []args){
 
-        String []unidade = {"zero", "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove"};
-        String []casaDez = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"};
-        String []dezenas = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
+  /*
+   * Escreva um programa que leia as três notas de um aluno, calcule e escreva a
+   * sua média. Este programa deverá permitir escolher o tipo de média a ser
+   * aplicada que será média aritmética simples ou média ponderada. Se a escolha
+   * for média ponderada o programa deverá requisitar os pesos respectivos de cada
+   * nota.
+   */
 
-		Scanner scan = new Scanner(System.in);		
-		System.out.println("Informe a idade da pessoa: ");
-		int idade = scan.nextInt();		
-        String idadeExtenso = "";
+  public static void main(String[] args) {
 
-        if (idade >= 0 && idade <= 9)
-			idadeExtenso = unidade[idade];
-		else if (idade >= 10 && idade <= 19)
-			idadeExtenso = casaDez[idade - 10];
-		else if (idade >= 20 && idade <= 99) {
-			int tempId = idade / 10;
-			int tempUn = idade - (tempId * 10);	
-				
-			if (idade % 10 == 0)
-				idadeExtenso = dezenas[tempId - 2];
-			else
-				idadeExtenso = dezenas[tempId - 2] + " e " + unidade[tempUn];
-		}
+    Scanner scan = new Scanner(System.in);
 
-        System.out.println("Idade: " + idade + " ( "+  idadeExtenso + " " + ((idade == 1) ? "ano" : "anos") + " )");
-        
-        scan.close();
-	}
+    float nota1, nota2, nota3, media;
+
+    media = 0;
+
+    System.out.println("Informe a primeira nota:");
+    nota1 = scan.nextFloat();
+
+    System.out.println("Informe a segunda nota:");
+    nota2 = scan.nextFloat();
+
+    System.out.println("Informe a terceira nota:");
+    nota3 = scan.nextFloat();
+
+    scan.nextLine();
+    System.out.println("Informe o tipo da média (Aritimetica ou Ponderada): ");
+    String tipoMedia = scan.nextLine();
+
+    if (tipoMedia.equals("Aritimetica"))
+      media = (nota1 + nota2 + nota3) / 3;
+
+    if (tipoMedia.equals("Ponderada")) {
+      System.out.println("Informe o peso da primeira nota (" + nota1 + "): ");
+      float peso1 = scan.nextInt();
+
+      System.out.println("Informe o peso da primeira nota (" + nota2 + "): ");
+      float peso2 = scan.nextInt();
+
+      System.out.println("Informe o peso da primeira nota (" + nota3 + "): ");
+      float peso3 = scan.nextInt();
+
+      media = ((nota1 * peso1) + (nota2 * peso2) + (nota3 + peso3)) / 3;
+    }
+
+    System.out.println("Sua média é: " + media);
+
+    scan.close();
+  }
 }
